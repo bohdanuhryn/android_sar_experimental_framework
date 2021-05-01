c2b2d34fde9c70b5b287d8c5d4801e8715eba27d
bohdanuhryn <bohdanuhryn@gmail.com>
2021-05-01 17:46:35 +0300

    Add OnTimeAction callback to stress script

--- scripts/stress.ps1
+++ scripts/stress.ps1
@@ -5,12 +5,20 @@ function RunStressTest {
         [String] $WorkloadGenerator,
         [String] $Monitor,
         [Int32] $WorkloadDuration = 0,
-        [Int32] $PauseDuration = 0
+        [Int32] $PauseDuration = 0,
+        [String] $OnTimeAction = "",
+        [Int32] $ActionTime = 0
     )
     & $Cleaner
     $i = 0
     while ($i -ne $IntervalsCount) {
         Write-Host "Monitor iteration  #$i"
+        if ($i -gt 0 -and $ActionTime -gt 0 -and $OnTimeAction.Length -gt 0) {
+            $j = $i % $ActionTime
+            if ($j -eq 0) {
+                & $OnTimeAction
+            }
+        }
         if (($WorkloadDuration -gt 0) -and ($PauseDuration -gt 0)) {
             $j = $i % ($WorkloadDuration + $PauseDuration)
             if ($j -lt $WorkloadDuration) {
