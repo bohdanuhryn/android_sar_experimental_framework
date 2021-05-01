05097fb6cf81d03bef60a812af8f5ca8c9998de9
bohdanuhryn <bohdanuhryn@gmail.com>
2021-05-01 17:18:56 +0300

    Add workload and pause durations for stress test

--- scripts/stress.ps1
+++ scripts/stress.ps1
@@ -1,17 +1,24 @@
-function RunStressTest 
-{
+function RunStressTest {
     param (
         [Int32] $IntervalsCount,
         [String] $Cleaner,
         [String] $WorkloadGenerator,
-        [String] $Monitor
+        [String] $Monitor,
+        [Int32] $WorkloadDuration = 0,
+        [Int32] $PauseDuration = 0
     )
     & $Cleaner
     $i = 0
-    while ($i -ne $IntervalsCount)
-    {
+    while ($i -ne $IntervalsCount) {
         Write-Host "Monitor iteration  #$i"
-        & $WorkloadGenerator
+        if (($WorkloadDuration -gt 0) -and ($PauseDuration -gt 0)) {
+            $j = $i % ($WorkloadDuration + $PauseDuration)
+            if ($j -lt $WorkloadDuration) {
+                & $WorkloadGenerator
+            }
+        } else {
+            & $WorkloadGenerator
+        }
         & $Monitor
         $i = $i + 1
     }   
