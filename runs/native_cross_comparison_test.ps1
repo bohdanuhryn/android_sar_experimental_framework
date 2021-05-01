c1624b9a0d99a5da640157c6af9e5d748a0c61eb
bohdanuhryn <bohdanuhryn@gmail.com>
2021-05-01 21:55:39 +0300

    Fix stress test and update native cross platform run

--- runs/native_cross_comparison_test.ps1
+++ runs/native_cross_comparison_test.ps1
@@ -4,48 +4,26 @@ Set-Alias -Name adb -Value c:\Users\Bohdan\AppData\Local\Android\Sdk\platform-to
 . Current-Location\..\scripts\workload.ps1
 . Current-Location\..\scripts\monitor.ps1
 
-$packagesFlutter = "com.emkore.apps.cryptograph", "com.reflectlyApp", "com.hamilton.app"
-$packagesReact = "com.shinetext.shine", "com.pinterest", "com.skype.raider"
-
-$outputFlutterMultipleApps = "flutter_multiple_apps_test"
-$outputFlutterSingleApp = "flutter_single_app_test"
-$outputFlutterUsageWithGaps = "flutter_usage_with_gaps_test"
-
-$outputReactMultipleApps = "react_multiple_apps_test"
-$outputReactSingleApp = "react_single_app_test"
-$outputReactUsageWithGaps = "react_usage_with_gaps_test"
-
-$intervalsCount = 500
-
-$intervalDurationMs = 60 * 1000
-
-$intervalEventsCount = 200
-
-function RunCleaners
-{
+function RunCleaners {
     param ([String[]] $Packages)
     adb logcat -c
-    foreach ($p in $Packages)
-    {
+    foreach ($p in $Packages) {
         adb shell dumpsys gfxinfo $p reset
     }
 }
 
-function RunPackagesInitial
-{
+function RestartPackages {
     param ([String[]] $Packages)
     KillPackages -Packages $Packages
     RunPackages -Packages $Packages
 }
 
-function RunWorkloadGenerators
-{
+function RunWorkloadGenerators {
     param ([String[]] $Packages)
-    RunMonkey -Packages $Packages -DurationMs $intervalDurationMs -EventsCount $intervalEventsCount
+    RunMonkey -Packages $Packages -DurationMs 60000 -EventsCount 200
 }
 
-function RunMonitors
-{
+function RunMonitors {
     param (
         [String] $Output,
         [String[]] $Packages
@@ -57,40 +35,82 @@ function RunMonitors
     ProcTasksMonitor -Output $Output
 }
 
+### Native
+
+$packagesNative = "com.google.android.youtube", "com.android.chrome", "com.android.camera", "com.google.android.apps.photos", "com.android.contacts"
 $outputNativeMultipleApps = "native_multiple_apps_test"
-$packagesNativeMultipleApps = "com.google.android.youtube", "com.android.chrome", "com.android.camera", "com.google.android.apps.photos"
-function RunNativeMultipleAppsCleaners { RunCleaners -Packages $packagesNativeMultipleApps }
-function RunNativeMultipleAppsWorkloadGenerators { RunWorkloadGenerators -Packages $packagesNativeMultipleApps }
-function RunNativeMultipleAppsMonitors { RunMonitors -Output $outputNativeMultipleApps -Packages $packagesNativeMultipleApps }
+$outputNativeUsageDelays = "native_usage_delays_test"
+function RestartNativeApps { RestartPackages -Packages $packagesNative }
+function RunNativeCleaners { RunCleaners -Packages $packagesNative }
+function RunNativeWorkload { RunWorkloadGenerators -Packages $packagesNative }
+function RunNativeMultipleAppsMonitor { RunMonitors -Output $outputNativeMultipleApps -Packages $packagesNative }
+function RunNativeUsageDelaysMonitor { RunMonitors -Output $outputNativeUsageDelays -Packages $packagesNative }
 function RunNativeMultipleAppsTest
 {
-    InitOutputDirs -Output $outputNativeMultipleApps -Packages $packagesNativeMultipleApps
-    RunPackagesInitial -Packages $packagesNativeMultipleApps
-    RunStressTest -IntervalsCount $intervalsCount -Cleaner 'RunNativeMultipleAppsCleaners' -WorkloadGenerator 'RunNativeMultipleAppsWorkloadGenerators' -Monitor 'RunNativeMultipleAppsMonitors'   
+    InitOutputDirs -Output $outputNativeMultipleApps -Packages $packagesNative
+    RestartPackages -Packages $packagesNative
+    RunStressTest -IntervalsCount 600 -Cleaner 'RunNativeCleaners' -WorkloadGenerator 'RunNativeWorkload' -Monitor 'RunNativeMultipleAppsMonitor' -ActionTime 200 -OnTimeAction 'RestartNativeApps'
 }
-
-function RunNativeSingleAppTest
+function RunNativeUsageDelaysTest
 {
-    $packages = "com.android.chrome"
-    $output = "native_single_app_test"
-    InitOutputDirs -Output $output -Packages $packages
-    RunPackages -Packages $packages
-    $RunCleaners = { RunCleaners -Packages $packages }
-    $RunWorkloadGenerators = { RunWorkloadGenerators -Packages $packages }
-    $RunMonitors = { RunMonitors -Outputs $output -Packages $packages }
-    RunStressTest -IntervalsCount $intervalsCount -Cleaner $RunCleaners -WorkloadGenerator $RunWorkloadGenerators -Monitor $RunMonitors   
+    InitOutputDirs -Output $outputNativeMultipleApps -Packages $packagesNativeApps
+    RestartPackages -Packages $packagesNativeApps
+    RunStressTest -IntervalsCount 400 -Cleaner 'RunNativeCleaners' -WorkloadGenerator 'RunNativeWorkload' -Monitor 'RunNativeUsageDelaysMonitor' -ActionTime 150 -OnTimeAction 'RestartNativeApps' -WorkloadDurationTicks 100 -PauseDurationSeconds 30 * 60
 }
 
-function RunNativeUsageWithGaps
+### Flutter
+
+$packagesFlutter = "com.reflectlyApp", "com.hamilton.app", "com.ebay.motorsapp", "com.spotlightsix.zentimerlite2", "com.grab.merchant"#, "com.google.stadia.android"
+$outputFlutterMultipleApps = "flutter_multiple_apps_test"
+$outputFlutterUsageDelays = "flutter_usage_delays_test"
+function RestartFlutterApps { RestartPackages -Packages $packagesFlutter }
+function RunFlutterCleaners { RunCleaners -Packages $packagesFlutter }
+function RunFlutterWorkload { RunWorkloadGenerators -Packages $packagesFlutter }
+function RunFlutterMultipleAppsMonitor { RunMonitors -Output $outputFlutterMultipleApps -Packages $packagesFlutter }
+function RunFlutterUsageDelaysMonitor { RunMonitors -Output $outputFlutterUsageDelays -Packages $packagesFlutter }
+function RunFlutterMultipleAppsTest
 {
-    $packages = "com.android.chrome"
-    $output = "native_usage_with_gaps_test"
-    InitOutputDirs -Output $output -Packages $packages
-    RunPackages -Packages $packages
-    $RunCleaners = { RunCleaners -Packages $packages }
-    $RunWorkloadGenerators = { RunWorkloadGenerators -Packages $packages }
-    $RunMonitors = { RunMonitors -Outputs $output -Packages $packages }
-    RunStressTest -IntervalsCount $intervalsCount -Cleaner $RunCleaners -WorkloadGenerator $RunWorkloadGenerators -Monitor $RunMonitors   
+    InitOutputDirs -Output $outputFlutterMultipleApps -Packages $packagesFlutter
+    RestartPackages -Packages $packagesFlutter
+    RunStressTest -IntervalsCount 600 -Cleaner 'RunFlutterCleaners' -WorkloadGenerator 'RunFlutterWorkload' -Monitor 'RunFlutterMultipleAppsMonitor'  -ActionTime 200 -OnTimeAction 'RestartFlutterApps'
+}
+function RunFlutterUsageDelaysTest
+{
+    InitOutputDirs -Output $outputFlutterMultipleApps -Packages $packagesFlutter
+    RestartPackages -Packages $packagesFlutter
+    RunStressTest -IntervalsCount 400 -Cleaner 'RunFlutterCleaners' -WorkloadGenerator 'RunFlutterWorkload' -Monitor 'RunFlutterUsageDelaysMonitor' -ActionTime 150 -OnTimeAction 'RestartFlutterApps' -WorkloadDurationTicks 100 -PauseDurationSeconds 30 * 60 
 }
 
-RunNativeMultipleAppsTest
+### React Native
+
+$outputReactMultipleApps = "react_multiple_apps_test"
+$packagesReactMultipleApps = "com.shinetext.shine", "com.pinterest", "com.cleevio.spendee", "com.myntra.android"#, "com.edutapps.maphi"
+function RunReactMultipleAppsCleaners { RunCleaners -Packages $packagesReactMultipleApps }
+function RunReactMultipleAppsWorkloadGenerators { RunWorkloadGenerators -Packages $packagesReactMultipleApps }
+function RunReactMultipleAppsMonitors { RunMonitors -Output $outputReactMultipleApps -Packages $packagesReactMultipleApps }
+function RunReactMultipleAppsTest
+{
+    InitOutputDirs -Output $outputReactMultipleApps -Packages $packagesReactMultipleApps
+    RestartPackages -Packages $packagesReactMultipleApps
+    RunStressTest -IntervalsCount 600 -Cleaner 'RunReactMultipleAppsCleaners' -WorkloadGenerator 'RunReactMultipleAppsWorkloadGenerators' -Monitor 'RunReactMultipleAppsMonitors'
+}
+
+### Entry point
+
+Write-Host "Run test:
+    - native   multiple apps  (1)
+    - native   usage delay    (11)
+    - flutter  multiple apps  (2)
+    - flutter  usage delay    (22)
+    - react    multiple apps  (3)
+    - react    usage delay    (33)"
+$run_code = Read-Host -Prompt 'Input run code:'
+
+switch ($run_code) {
+    1 { RunNativeMultipleAppsTest; Break }
+    11 { RunNativeUsageDelaysTest; Break }
+    2 { RunFlutterMultipleAppsTest; Break }
+    22 { RunFlutterUsageDelaysTest; Break }
+    3 { RunReactMultipleAppsTest; Break }
+    Default { Write-Host "Ooops! :)" }
+}
--- scripts/stress.ps1
+++ scripts/stress.ps1
@@ -4,8 +4,8 @@ function RunStressTest {
         [String] $Cleaner,
         [String] $WorkloadGenerator,
         [String] $Monitor,
-        [Int32] $WorkloadDuration = 0,
-        [Int32] $PauseDuration = 0,
+        [Int32] $WorkloadDurationTicks = 0,
+        [Int32] $PauseDurationSeconds = 0,
         [String] $OnTimeAction = "",
         [Int32] $ActionTime = 0
     )
@@ -13,15 +13,21 @@ function RunStressTest {
     $i = 0
     while ($i -ne $IntervalsCount) {
         Write-Host "Monitor iteration  #$i"
-        if ($i -gt 0 -and $ActionTime -gt 0 -and $OnTimeAction.Length -gt 0) {
+        if (($i -gt 0) -and ($ActionTime -gt 0) -and ($OnTimeAction.Length -gt 0)) {
             $j = $i % $ActionTime
             if ($j -eq 0) {
                 & $OnTimeAction
             }
         }
-        if (($WorkloadDuration -gt 0) -and ($PauseDuration -gt 0)) {
-            $j = $i % ($WorkloadDuration + $PauseDuration)
-            if ($j -lt $WorkloadDuration) {
+        if (($i -gt 0) -and ($WorkloadDurationTicks -gt 0) -and ($PauseDurationSeconds -gt 0)) {
+            if (($i % $WorkloadDurationTicks) -eq 0) {
+                $sec = 0
+                while ($sec -lt $PauseDurationSeconds) {
+                    Start-Sleep -s 25
+                    & $Monitor
+                    $sec = $sec + 30#TODO: fix pause timing - remove hardcoded 20/30
+                }
+            } else {
                 & $WorkloadGenerator
             }
         } else {
